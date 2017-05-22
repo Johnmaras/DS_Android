44ea282ca9dff78f9bf00c73fd73426a6a962fe2
John <mitromarasg96@gmail.com>
2017-05-22 22:22:14 +0300

    Coordinates added. Not ready

---
+++ app/src/main/java/com/example/john/testing_the_maps/Coordinates.java
@@ -0,0 +1,12 @@
+package com.example.john.testing_the_maps;
+
+public class Coordinates {
+
+    private LatLngAdapter origin;
+    private LatLngAdapter destination;
+
+    public Coordinates(LatLngAdapter origin, LatLngAdapter destination){
+        this.origin = origin;
+        this.destination = destination;
+    }
+}
