15223b79e8cbc00bac68c7a4bef207a4f7017a03
John <mitromarasg96@gmail.com>
2017-05-22 22:24:21 +0300

    LatLngAdapter, PolylineAdapter added

---
+++ app/src/main/java/com/example/john/testing_the_maps/PolylineAdapter.java
@@ -0,0 +1,20 @@
+package com.example.john.testing_the_maps;
+
+import java.util.ArrayList;
+
+public class PolylineAdapter{
+    private final ArrayList<LatLngAdapter> points = new ArrayList<>();
+
+    public ArrayList<LatLngAdapter> getPoints() {
+        return points;
+    }
+
+    public void addPoint(double latitude, double longitude){
+        LatLngAdapter newPoint = new LatLngAdapter(latitude, longitude);
+        points.add(newPoint);
+    }
+
+    public void addPoint(LatLngAdapter latlngPoint){
+        points.add(latlngPoint);
+    }
+}
