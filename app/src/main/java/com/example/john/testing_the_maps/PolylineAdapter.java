37e0e0ad11e13795220f0ae42f49b4c8453b3577
John <mitromarasg96@gmail.com>
2017-05-29 22:49:06 +0300

    addAllPoint

--- app/src/main/java/com/example/john/testing_the_maps/PolylineAdapter.java
+++ app/src/main/java/com/example/john/testing_the_maps/PolylineAdapter.java
@@ -1,6 +1,7 @@
 package com.example.john.testing_the_maps;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PolylineAdapter{
     private final ArrayList<LatLngAdapter> points = new ArrayList<>();
@@ -14,7 +15,11 @@ public class PolylineAdapter{
         points.add(newPoint);
     }
 
-    public void addPoint(LatLngAdapter latlngPoint){
-        points.add(latlngPoint);
+    public void addPoint(LatLngAdapter point){
+        points.add(point);
+    }
+
+    public void addAllPoint(List<LatLngAdapter> point){
+        points.addAll(point);
     }
 }
