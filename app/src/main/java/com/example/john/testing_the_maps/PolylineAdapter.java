251a2d838eb55caf9bdcda165ab12fcf5d6e8dcb
Johnmaras <giannis19961@windowslive.com>
2017-06-15 02:22:33 +0300

    minor adds

--- app/src/main/java/com/example/john/testing_the_maps/PolylineAdapter.java
+++ app/src/main/java/com/example/john/testing_the_maps/PolylineAdapter.java
@@ -1,9 +1,20 @@
 package com.example.john.testing_the_maps;
 
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
-public class PolylineAdapter{
+public class PolylineAdapter implements Serializable{
+
+    private static final long serialVersionUID = 7314160120198237281L;
+    //TODO assign values
+    /**
+     * full precision values
+     */
+    private LatLngAdapter origin;
+    private LatLngAdapter destination;
+    private static boolean first = true;
+
     private final ArrayList<LatLngAdapter> points = new ArrayList<>();
 
     public ArrayList<LatLngAdapter> getPoints() {
@@ -13,14 +24,31 @@ public class PolylineAdapter{
     public void addPoint(double latitude, double longitude){
         LatLngAdapter newPoint = new LatLngAdapter(latitude, longitude);
         points.add(newPoint);
+        if(first) {
+            origin = newPoint;
+            first = false;
+        }
+        destination = newPoint;
     }
 
-    public void addPoint(LatLngAdapter point){
-        points.add(point);
+    public LatLngAdapter getOrigin(){
+        return this.origin;
     }
 
-    public void addAllPoint(List<LatLngAdapter> point){
-        points.addAll(point);
+    public LatLngAdapter getDestination() {
+        return this.destination;
+    }
+
+    public void addPoint(LatLngAdapter latlngPoint){
+        points.add(latlngPoint);
+        origin = points.get(0);
+        destination = points.get(points.size() - 1);
+    }
+
+    public void addAllPoint(Iterable<LatLngAdapter> points){
+        this.points.addAll((Collection<? extends LatLngAdapter>) points);
+        origin = this.points.get(0);
+        destination = this.points.get(this.points.size() - 1);
     }
 
     public boolean isEmpty(){
