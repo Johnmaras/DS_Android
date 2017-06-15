1abec51543f60f829d78d9328231fcd7363f3f8c
Johnmaras <giannis19961@windowslive.com>
2017-06-15 21:11:09 +0300

    the outer JsonElement is JsonObject not JsonArray

--- app/src/main/java/com/example/john/testing_the_maps/PolylineAdapterDeserializer.java
+++ app/src/main/java/com/example/john/testing_the_maps/PolylineAdapterDeserializer.java
@@ -1,6 +1,5 @@
 package com.example.john.testing_the_maps;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -14,8 +13,8 @@ public class PolylineAdapterDeserializer implements JsonDeserializer{
 
     @Override
     public Object deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonContext) throws JsonParseException {
-        JsonArray array = jsonElement.getAsJsonArray();
-        JsonObject object = array.get(0).getAsJsonObject();
+        //JsonArray array = jsonElement.getAsJsonArray();
+        JsonObject object = jsonElement.getAsJsonObject();
 
         /*LatLngAdapter origin = jsonContext.deserialize(object.get("origin"), LatLngAdapter.class);
         LatLngAdapter destination = jsonContext.deserialize(object.get("destination"), LatLngAdapter.class);*/
